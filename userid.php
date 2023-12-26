6d3e66798930b8c76ed7a88067dd73630d93dec8
snpsuvrat <suvrat.pandey.9@gmail.com>
2023-12-26 23:01:57 +0530

    hello

---
+++ userid.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="userid.php" method="POST">
+        <label>Input Username</label>
+        <input type="text" name="id"/>
+        <br>
+        <label>Input Password</label>
+        <input type="password" name="pass"/>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
+<?php
+$id=$_POST['id'];
+$pass=$_POST['pass'];
+if($id== "suvrat")
+{
+    if($pass== "123")
+    {
+        echo "Login Successfull";
+    }
+    else{
+        echo "Wrong Password";
+    }
+}
+else {
+    echo "Login Failed";
+}
+?>
